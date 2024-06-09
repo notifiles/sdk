5dd284cdaa81c545a4f911944b395a00ea28092e
Aboubacar Doucoure <yelounak@gmail.com>
2024-06-09 15:23:24 +0200

    fix: added ensureDirectoryExists

--- src/template/new/index.js
+++ src/template/new/index.js
@@ -9,6 +9,7 @@ import emailTemplateMaster from './templates/email.mastered.js'
 import textmessageTemplate from './templates/textmessage.js'
 import pushnotificationTemplate from './templates/pushnotification.js'
 import _slug from 'slug'
+import ensureDirectoryExists from '../../lib/fs/ensureDirectoryExists.js'
 
 export default async ({
   path,
@@ -28,6 +29,8 @@ export default async ({
       }
       await fs.promises.rmdir(folderPath, { recursive: true })
     }
+
+    await ensureDirectoryExists(folderPath)
 
     await fs.promises.mkdir(folderPath)
     await fs.promises.mkdir(`${folderPath}/attachments`)
