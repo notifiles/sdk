eb22929ac7b7ce32c76f7635d83d11726ee30458
Aboubacar Doucoure <yelounak@gmail.com>
2025-12-01 10:33:05 +0100

    feat: added process env in json i18n

--- src/lib/fs/importJSONAsync.js
+++ src/lib/fs/importJSONAsync.js
@@ -1,6 +1,28 @@
 import fs from "fs"
 import checkFileExists from "./checkFileExists.js"
 
+const expandEnvPlaceholders = (value) => {
+  if (typeof value === 'string') {
+    return value.replace(/\$\{process\.env\.([A-Z0-9_]+)\}/g, (match, envKey) => {
+      const envValue = process.env[envKey]
+      return envValue !== undefined ? envValue : match
+    })
+  }
+
+  if (Array.isArray(value)) {
+    return value.map(expandEnvPlaceholders)
+  }
+
+  if (value && typeof value === 'object') {
+    return Object.entries(value).reduce((acc, [key, val]) => {
+      acc[key] = expandEnvPlaceholders(val)
+      return acc
+    }, {})
+  }
+
+  return value
+}
+
 export default async _url => {
   // try {
   if (!(await checkFileExists(_url))) {
@@ -8,7 +30,7 @@ export default async _url => {
   }
   const fileUrl = new URL(_url, import.meta.url)
   const parsedPackageJSON = JSON.parse(await fs.promises.readFile(fileUrl, 'utf8'))
-  return parsedPackageJSON
+  return expandEnvPlaceholders(parsedPackageJSON)
   // } catch (e) {
   //   console.log("[notifiles]", `importJSONAsync → `, e)
   //   return null
